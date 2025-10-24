01cfd1d718e2fe26fd1b6fe2a2cf2efee4ab86ac
Mark Handy <mjhandy@outlook.com>
2025-10-24 08:39:21 -0300

    * added Material * implementation of mat-icon for site logo * header updates for new nav location

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
+
 import { Header } from './components/global/header/header';
 import { Footer } from './components/global/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, MatIconModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('dmg');
+  private iconPath = '/assets/';
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+
+    private domSanitizer: DomSanitizer,
+  ) {
+
+    this.matIconRegistry
+      .addSvgIcon(
+        "site-logo",
+        this.setPath(`${this.iconPath}logo_horizontal.svg`)
+      );
+
+
+  }
+
+
+  setPath(url: string): SafeResourceUrl {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
 }
--- src/app/components/global/header/logo/logo.ts
+++ src/app/components/global/header/logo/logo.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
-  imports: [],
+  imports: [MatIconModule, RouterModule],
   templateUrl: './logo.html',
   styleUrl: './logo.scss'
 })
