31a73975afa705c13a76175427b3244f2f74d80b
Mark J. Handy <mjhandy@outlook.com>
2025-10-25 19:39:54 -0300

    tweaks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,25 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./components/pages/home/home').then(m => m.Home),
-    title: 'English Home Page | Dundee Media Group',
+    title: 'Home Page | Dundee Media Group',
     data: {
       desciption: 'This is the Web Prototype home page',
-      label: 'nav.home',
+      label: 'Home',
+      showInMain: false,
       showInFooter: true,
       showInSiteMap: true,
     }
   },
+  {
+    path: 'services',
+    loadComponent: () => import('./components/pages/home/home').then(m => m.Home),
+    title: 'Services | Dundee Media Group',
+    data: {
+      desciption: 'This is the Web Prototype home page',
+      label: 'Services',
+      showInMain: true,
+      showInFooter: true,
+      showInSiteMap: true,
+    }
+  },  
 ];
