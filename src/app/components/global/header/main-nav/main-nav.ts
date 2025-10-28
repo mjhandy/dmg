a2d708aca5c31bc242e618bf4ca61df5c48a84a3
Mark Handy <mjhandy@outlook.com>
2025-10-28 11:12:25 -0300

    * VS Code settings * updated favicon * code spacing

--- src/app/components/global/header/main-nav/main-nav.ts
+++ src/app/components/global/header/main-nav/main-nav.ts
@@ -6,29 +6,30 @@ import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-main-nav',
-  imports: [Logo, MatIconModule,RouterModule ],
+  imports: [Logo, MatIconModule, RouterModule],
   templateUrl: './main-nav.html',
   styleUrl: './main-nav.scss'
 })
 export class MainNav {
- routes: { 
-    path:   string; 
-    title:  string;  
-    label:  string;
+  routes: {
+    path: string;
+    title: string;
+    label: string;
   }[] = [];
 
   constructor(private router: Router) {
     // Filter routes that have a valid `path` and `title`, then map to the required type
     this.routes = this.router.config
       .filter(
-        route => route.path && 
-        typeof route.title === 'string' && 
-        route.data && route.data['label'] && 
-        route.data['showInFooter'] === true
+        route => route.path &&
+          typeof route.title === 'string' &&
+          route.data && route.data['label'] &&
+          route.data['showInFooter'] === true
       )
-      .map(route => ({ 
-        path: route.path!, 
-        title: route.title as string, 
-        label: route.data!['label'] }));
+      .map(route => ({
+        path: route.path!,
+        title: route.title as string,
+        label: route.data!['label']
+      }));
   }
 }
