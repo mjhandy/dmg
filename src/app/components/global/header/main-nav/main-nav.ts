3cf1ddb5a9b665e9b1def9021dd53e83a1406f0c
Mark Handy <mjhandy@outlook.com>
2025-10-28 10:29:16 -0300

    * mobile nav UI

--- src/app/components/global/header/main-nav/main-nav.ts
+++ src/app/components/global/header/main-nav/main-nav.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Logo } from '../logo/logo';
 
+import { MatIconModule } from '@angular/material/icon';
+
+
 @Component({
   selector: 'app-main-nav',
-  imports: [Logo ],
+  imports: [Logo, MatIconModule ],
   templateUrl: './main-nav.html',
   styleUrl: './main-nav.scss'
 })
