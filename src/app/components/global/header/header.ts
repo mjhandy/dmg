4a40d9d1d61a7fa694c7e96848113a7d83ab2f42
Mark Handy <mjhandy@outlook.com>
2025-10-28 09:17:23 -0300

    * mobile nav off canvas

--- src/app/components/global/header/header.ts
+++ src/app/components/global/header/header.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-
-import { Logo } from './logo/logo';
 import { MainNav } from './main-nav/main-nav';
 
 
 @Component({
   selector: 'app-header',
-  imports: [Logo, MainNav],
+  imports: [MainNav],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
--- src/app/components/global/header/main-nav/main-nav.ts
+++ src/app/components/global/header/main-nav/main-nav.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { Logo } from '../logo/logo';
 
 @Component({
   selector: 'app-main-nav',
-  imports: [],
+  imports: [Logo ],
   templateUrl: './main-nav.html',
   styleUrl: './main-nav.scss'
 })
