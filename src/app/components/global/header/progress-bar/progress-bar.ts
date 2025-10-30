d3bb5e8d418cd9013b68ae0acfacbf816c918251
Mark J. Handy <mjhandy@outlook.com>
2025-10-30 17:51:59 -0300

    scroll progress bar

--- src/app/components/global/header/progress-bar/progress-bar.ts
+++ src/app/components/global/header/progress-bar/progress-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './progress-bar.scss'
 })
 export class ProgressBar {
+
+divWidth: number = 0; // Initial width
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const scrollTop  = window.scrollY;
+    const viewportHeight   = window.innerHeight; // Height of the viewport
+    const scrollPercentage = (scrollTop / viewportHeight) * 100; // Percentage of viewport height
+
+
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrolled = (winScroll / height) * 100;
+    console.debug(scrolled);
+    // this.divWidth = 1 + scrollPosition  /  2; // Adjust multiplier as needed
+    this.divWidth = scrolled;
+  }
 
 }
