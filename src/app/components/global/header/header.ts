169c6abebd3e5fe0df5a23c2249834568af42a78
Mark Handy <mjhandy@outlook.com>
2025-10-30 17:23:12 -0300

    * scroll position indicator

--- src/app/components/global/header/header.ts
+++ src/app/components/global/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainNav } from './main-nav/main-nav';
+import { ProgressBar } from './progress-bar/progress-bar';
 
 
 @Component({
   selector: 'app-header',
-  imports: [MainNav],
+  imports: [MainNav, ProgressBar],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
