672ecf3b319ac123fbbe96732f0cb9972822f689
Mark Handy <mjhandy@outlook.com>
2025-10-31 09:40:38 -0300

    * Hero banner updates

--- src/app/components/global/hero-banner/hero-banner.ts
+++ src/app/components/global/hero-banner/hero-banner.ts
@@ -1,10 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-banner',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-banner.html',
   styleUrl: './hero-banner.scss'
 })
