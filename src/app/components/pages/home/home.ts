2bab7d85f3c345f8fef45a4afc806b72a5c9651e
Mark Handy <mjhandy@outlook.com>
2025-11-03 08:05:32 -0400

    * added open and closed quotes * added testimonials to home page * slight typography update

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { HeroBanner } from '../../global/hero-banner/hero-banner';
+import { TestimonialsService } from '../../../services/testimonials';
+import { Testimonials } from '../../global/testimonials/testimonials';
 @Component({
   selector: 'app-home',
-  imports: [HeroBanner],
+  imports: [HeroBanner, Testimonials],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
   parentComp = "page-home";
+  review: any;
 
+    constructor(
+    private testimonialService: TestimonialsService
+  ){}
+
+  ngOnInit(){
+    this.testimonialService.getTestimonials()
+      .subscribe(Response => {
+        this.review = Response;
+      })
+  }
 
 }
