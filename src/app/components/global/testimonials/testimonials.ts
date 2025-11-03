bcb02139585551ca76dab6b5d87beca5a982b86b
Mark J. Handy <mjhandy@outlook.com>
2025-11-02 20:18:14 -0400

    added buttons

--- src/app/components/global/testimonials/testimonials.ts
+++ src/app/components/global/testimonials/testimonials.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-testimonials',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './testimonials.html',
   styleUrl: './testimonials.scss'
 })
