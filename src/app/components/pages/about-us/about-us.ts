c3ae6130830898d0207536500ab76359d21aa28b
Mark J. Handy <mjhandy@outlook.com>
2025-11-06 17:31:58 -0400

    About US
    * updated copy
    * add tesimonials

--- src/app/components/pages/about-us/about-us.ts
+++ src/app/components/pages/about-us/about-us.ts
@@ -2,12 +2,27 @@ import { Component } from '@angular/core';
 
 import { HeroBanner } from '../../global/hero-banner/hero-banner';
 
+import { TestimonialsService } from '../../../services/testimonials';
+import { Testimonials } from '../../global/testimonials/testimonials';
+
 @Component({
   selector: 'app-about-us',
-  imports: [HeroBanner],
+  imports: [HeroBanner, Testimonials],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss'
 })
 export class AboutUs {
   parentComp = "page-aboutUs";
+  review: any;
+
+    constructor(
+    private testimonialService: TestimonialsService
+  ){}
+
+  ngOnInit(){
+    this.testimonialService.getTestimonials()
+      .subscribe(Response => {
+        this.review = Response;
+      })
+  }  
 }
