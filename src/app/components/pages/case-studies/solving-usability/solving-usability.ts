cfe00e3bda4bec1be082bc45d74c9b4a0c1e44d2
Mark J. Handy <mjhandy@outlook.com>
2025-11-07 11:22:17 -0400

    * minor code fix, missing css class

--- src/app/components/pages/case-studies/solving-usability/solving-usability.ts
+++ src/app/components/pages/case-studies/solving-usability/solving-usability.ts
@@ -1,12 +1,11 @@
 
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { HeroBanner } from '../../../global/hero-banner/hero-banner';
 
 @Component({
   selector: 'app-solving-usability',
   standalone: true,
-  imports: [HeroBanner, RouterModule],
+  imports: [ RouterModule],
   templateUrl: './solving-usability.html',
   styleUrl: './solving-usability.scss'
 })
