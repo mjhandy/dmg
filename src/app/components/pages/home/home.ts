9d0b953ae0cb567527b5057ef18282747a3ad000
Mark J. Handy <mjhandy@outlook.com>
2025-11-25 16:37:08 -0400

    Home Page
    * service cards, just need images

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -4,13 +4,14 @@ import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { HeroBanner } from '../../global/hero-banner/hero-banner';
 
+import { CardsServices } from '../../global/cards/cards-services/cards-services';
 import { CardsCasestudies } from '../../global/cards/cards-casestudies/cards-casestudies';
 
 import { TestimonialsService } from '../../../services/testimonials';
 import { Testimonials } from '../../global/testimonials/testimonials';
 @Component({
   selector: 'app-home',
-  imports: [HeroBanner, Testimonials, CardsCasestudies, MatButton, MatIcon],
+  imports: [HeroBanner, Testimonials, CardsCasestudies, CardsServices, MatButton, MatIcon],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
