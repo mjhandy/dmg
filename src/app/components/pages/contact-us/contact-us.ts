d9a83f93be5cdf5fbbadfce2359da0bca3b62674
Mark J. Handy <mjhandy@outlook.com>
2025-11-27 17:25:15 -0400

    Form update
    * external JSON data

--- src/app/components/pages/contact-us/contact-us.ts
+++ src/app/components/pages/contact-us/contact-us.ts
@@ -1,24 +1,21 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { HeroBanner } from '../../global/hero-banner/hero-banner';
+import { formConfig } from './form-config';
 
 
 @Component({
   selector: 'app-contact-us',
-  imports: [HeroBanner, ReactiveFormsModule,],
+  imports: [HeroBanner, ReactiveFormsModule, ],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.scss'
 })
 export class ContactUs {
   parentComp = "page-contactUs";
   form!: FormGroup;
+  ff = formConfig;
   field: any;
-
-  formConfig = [
-    { label: 'Name', inputType: 'text' },
-    { label: 'Email', inputType: 'email' },
-  ];
 
   constructor(
     private fb: FormBuilder
@@ -26,11 +23,11 @@ export class ContactUs {
 
   ngOnInit() {
     const group: any = {};
-    this.formConfig.forEach((field: { label: string | number; }) => {
+    this.ff.forEach((field: { label: string | number; }) => {
       group[field.label] = new FormControl('');
     });
     this.form = new FormGroup(group);
-    console.debug(this.formConfig);
+    console.debug(this.ff);
   }
 
   submitForm() {
