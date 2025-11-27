6e44301897a58398c21f37dc985bf017970e0874
Mark J. Handy <mjhandy@outlook.com>
2025-11-27 10:16:20 -0400

    * very basic form fields

--- src/app/components/pages/contact-us/contact-us.ts
+++ src/app/components/pages/contact-us/contact-us.ts
@@ -1,21 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+
 import { HeroBanner } from '../../global/hero-banner/hero-banner';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { formConfig } from './form-config';
+
 
 @Component({
   selector: 'app-contact-us',
-  imports: [HeroBanner],
+  imports: [HeroBanner, ReactiveFormsModule,],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.scss'
 })
 export class ContactUs {
   parentComp = "page-contactUs";
+  form!: FormGroup;
+  field: any;
+
+  formConfig = [
+    { label: 'Name', inputType: 'text' },
+    { label: 'Email', inputType: 'email' },
+  ];
 
   constructor(
-    
+    private fb: FormBuilder
   ) { }
 
-  
+  ngOnInit() {
+    const group: any = {};
+    this.formConfig.forEach((field: { label: string | number; }) => {
+      group[field.label] = new FormControl('');
+    });
+    this.form = new FormGroup(group);
+    console.debug(this.formConfig);
+  }
+
+  submitForm() {
+    console.log(this.form.value);
+  }
+
 
 }
